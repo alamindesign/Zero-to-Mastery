9f0296527e50d16c969fe6829e06e34b178bd67a
alamindesign <mail.muhammadit@gmail.com>
2022-12-23 22:33:26 +0600

    function exits at return

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+ function sayHello(){
+    console.log("Hello");
+ }
+
+ var sayBye = function() {
+    console.log("Bye");
+ }
+
+function multiply(a,b){
+    return a*b;
+    return a;
+    return b;
+}
+console.log(multiply(5,10));
