4c151aefe99b6eb96d7080a86051f7f1f91c2245
alamindesign <mail.muhammadit@gmail.com>
2022-12-23 23:04:19 +0600

    more about array

--- javascript/main.js
+++ javascript/main.js
@@ -7,8 +7,18 @@
  }
 
 function multiply(a,b){
-    return a*b;
-    return a;
-    return b;
+    if (a>10 || b>10){
+        return "That's too hard";
+    }else{
+        return a*b;
+    }
 }
-console.log(multiply(5,10));
+console.log(multiply(5,11));
+// Array
+var list = [["tiger","cat","bear","bird"],["Rakib","Niloy","Hridoy","Sumon"]];
+console.log(list[0][2],list[1][0]);
+var myList = ["dog","cat","Monkey"];
+var myNewList = myList.concat(["tom","jerry"]);
+console.log(myList);
+//concat([]) creates new array with the old one...
+console.log(myNewList);
