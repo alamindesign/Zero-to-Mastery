7e0f19161e4f0ecba720133f4897683fd97c5363
alamindesign <mail.muhammadit@gmail.com>
2022-12-24 07:52:20 +0600

    learning array method

---
+++ javascript/Array.js
@@ -0,0 +1,21 @@
+// Array method
+/*
+pop(),
+push(),
+shift(),
+unshift(),
+concat([]),
+splice(,),
+slice(,),
+sort(),
+forEach(),
+isArray(),
+toString(),
+join()
+*/
+//using concat array mathod
+let fruits = ['Mango','Apple','Banana','Goaba'];
+let sweets = ['Roshogolla','Kalo jam','Ponch'];
+let cookies = ['Dry cake','Solti','Sweet'];
+let items = fruits.concat(sweets,cookies);
+console.log(items);
