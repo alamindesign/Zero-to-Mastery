134385a579486b41920ddce87551bcf42a5e3fb5
alamindesign <mail.muhammadit@gmail.com>
2022-12-24 12:22:33 +0600

    learning object

--- javascript/Array.js
+++ javascript/Array.js
@@ -15,7 +15,11 @@ join()
 */
 //using concat array mathod
 let fruits = ['Mango','Apple','Banana','Goaba'];
+fruits.splice(2,0,"Licchi","Watermilon");
 let sweets = ['Roshogolla','Kalo jam','Ponch'];
 let cookies = ['Dry cake','Solti','Sweet'];
 let items = fruits.concat(sweets,cookies);
 console.log(items);
+items.forEach(function(item){
+    console.log("*",item);
+})
---
+++ javascript/object.js
@@ -0,0 +1,21 @@
+var user = {
+    name: "Jhon",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
+    spells: ["abrakadraba","shazam","boo"],
+    shout: function(){
+        console.log("AHHHHHHH!"); 
+    }
+};
+console.log(user);
+var list = [
+    {
+        username: "andy",
+        password: "secre"
+    },
+    {
+        username:"jess",
+        password: "123"
+    }
+]
