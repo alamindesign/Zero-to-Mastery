c299d73b83a57bdc0a05456f56b58c6d1237538c
alamindesign <mail.muhammadit@gmail.com>
2022-12-30 13:15:47 +0600

    insert list in the ul

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,11 +1,24 @@
 let input = document.getElementById("user-input");
 let button = document.getElementById("enter");
 let ul = document.querySelector("ul");
-button.addEventListener("click",function(){
-   if(input.value > '0'){
-      var li = document.createElement("li");
-      li.appendChild(document.createTextNode(input.value));
-      ul.appendChild(li);
-      input.value = null;
+function inputLength(){
+   return input.value.length;
+}
+function addElementTotheList(){
+   var li = document.createElement("li");
+   li.appendChild(document.createTextNode(input.value));
+   ul.appendChild(li);
+   input.value = null;
+}
+function addListAfterClick(){
+   if(inputLength() > 0){
+      addElementTotheList()
    }
-})
+}
+function addListAfterKeypress(event){
+   if(inputLength() > 0 && event.keyCode === 13){
+     addElementTotheList()
+   }
+}
+button.addEventListener("click", addListAfterClick)
+input.addEventListener("keypress", addListAfterKeypress)
